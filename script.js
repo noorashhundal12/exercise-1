17727eea67d621d12601a5cc4b84df5e356d5d44
madeeha ali <124605965+noorashhundal12@users.noreply.github.com>
2023-02-22 15:32:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("hello welcome to our site");
+document.getElementById("h1").innerHTML="Explore our website"
+
+function myfunction(){
+  alert("hello welcome to our site\n happy coding" );
+}
+
+
